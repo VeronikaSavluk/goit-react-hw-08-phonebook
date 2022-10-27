66c83f62657d873183c14aa5fd434710b6cfa430
Veronika Savluk <s_veronica@ukr.net>
2022-10-28 00:17:11 +0300

    Create Container.styled.js

---
+++ src/components/Container.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+display: flex;
+flex-direction: column;
+margin: 0;
+padding: 20px;
+width: 320px;
+min-height: 796px;
+background: #8FBC8F;
+`;
+export const Title = styled.h2`
+margin-botton: 10px;
+font-size: 18px;
+`;
