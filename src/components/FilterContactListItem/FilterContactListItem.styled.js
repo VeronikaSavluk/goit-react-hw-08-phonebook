e9d607f215fd5a7d659a13b99d3fd1f421d1e104
Veronika Savluk <s_veronica@ukr.net>
2022-10-28 00:17:18 +0300

    Create FilterContactListItem.styled.js

---
+++ src/components/FilterContactListItem/FilterContactListItem.styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ContactBook = styled.div`
+display: flex;
+flex-direction: column;
+margin: 0;
+padding: 10px;
+`;
+export const NameInput = styled.input`
+margin-bottom: 10px;
+padding: 4px;
+font-size: 12px;
+background: #FFF8DC;
+border: none;
+border-radius: 3px;
+;`
