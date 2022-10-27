4bcb25d37772c7e9e48386e255750b2528ee1b60
Veronika Savluk <s_veronica@ukr.net>
2022-10-28 00:17:00 +0300

    Create ContactList.styled.js

---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+padding: 10px;
+margin: 0;
+list-style: none;
+`;
+
+export const ContactItem = styled.li`
+display: grid;
+grid-template-columns: 155px 80px 65px;
+margin-bottom: 5px;
+padding: 0;
+`;
+
+export const DataContact = styled.p`
+margin: 0;
+padding: 0;
+`;
+
+export const Delete = styled.button`
+padding: 4px;
+margin: 0;
+width: 65px;
+font-size: 9px;
+border-radius: 15px;
+border-color: #1E90FF;
+background-color: #1E90FF;
+color: #FFF8DC;
+`
