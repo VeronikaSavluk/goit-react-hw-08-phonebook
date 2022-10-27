0797fecc87ccae54b9585d2251fe9a5d1cc29353
Veronika Savluk <s_veronica@ukr.net>
2022-10-28 00:18:34 +0300

    Create FilterContactListItem.js

---
+++ src/components/FilterContactListItem/FilterContactListItem.js
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
+import { setFilter } from "../../redux/filterSlice";
+import { nanoid } from "nanoid";
+
+import { ContactBook, NameInput } from "./FilterContactListItem.styled";
+import { NameLable } from "../ContactForm/ContactForm.styled";
+
+const FilterContactList = () => {
+    const QueryInputId = nanoid();
+    const dispatch = useDispatch();
+
+    const handleChange = e => {
+        e.preventDefault();
+        const form = e.target;
+        dispatch(setFilter(form.value.toLowerCase()));
+    };
+
+    return (
+        <ContactBook>
+            <NameLable htmlFor={QueryInputId}>Find contacts by name</NameLable>
+            <NameInput
+                type="text"
+                name="value"
+                id={QueryInputId}
+                onChange={handleChange}
+        />
+        </ContactBook>
+    )
+}
+
+FilterContactList.propTypes = {
+    handleChange: PropTypes.func,
+    QueryInputId: PropTypes.func,
+}
+
+export default FilterContactList;
