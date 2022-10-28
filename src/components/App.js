48d18d63efd416da7b84ec56e811bfce89153dd0
Veronika Savluk <s_veronica@ukr.net>
2022-10-29 02:24:08 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,32 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchContacts } from "redux/operations";
+import { selectorIsLoading, selectorError } from "redux/selectors";
 import ContactForm from './ContactForm';
 import FilterContactList from './FilterContactListItem';
+import Loader from "./Loader";
 import ContactList from './ContactList';
-import { Container, Title } from './Container.styled';
+import { Container, Title, LoaderPlace, ErrorMessage } from './Container.styled';
 
-function App () {
+function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectorIsLoading);
+  const error = useSelector(selectorError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <Container>
       <h1>Phonebook</h1>
       <ContactForm />
       <Title>Contacts</Title>
       <FilterContactList />
+      <LoaderPlace>
+        {isLoading && !error && <Loader />}
+        {error && <ErrorMessage>{error}</ErrorMessage>}
+      </LoaderPlace>
       <ContactList />
     </Container>
   );
