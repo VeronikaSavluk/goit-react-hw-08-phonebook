9d19ae867562c1cb91eeb34291a5d5895448adcc
Veronika Savluk <s_veronica@ukr.net>
2022-10-29 02:23:17 +0300

    Update Container.styled.js

--- src/components/Container.styled.js
+++ src/components/Container.styled.js
@@ -13,3 +13,11 @@ export const Title = styled.h2`
 margin-botton: 10px;
 font-size: 18px;
 `;
+
+export const LoaderPlace = styled.div`
+height: 18px;
+text-align: center;
+`;
+export const ErrorMessage = styled.p`
+ font-size: 16px;
+`;
