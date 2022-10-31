2d6db4bd9c783ad3f55bf2fa964dc235a3500670
Veronika Savluk <s_veronica@ukr.net>
2022-11-01 00:26:09 +0200

    Create LoginForm.js

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,68 @@
+// import { useDispatch } from "react-redux";
+import { nanoid } from "nanoid";
+import * as yup from "yup";
+import { ErrorMessage, Formik } from "formik";
+// import authOperations from "redux/auth/operations";
+import {
+    Wrapper,
+    NameLable,
+    Input,
+    ErrorBox,
+    Error,
+    SubitForm
+} from "../ContactForm/ContactForm.styled";
+
+const EmailInputId = nanoid();
+const PasswordInputId = nanoid();
+
+const initialValues = {
+    email: '',
+    password: ''
+};
+
+const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.mixed(),
+});
+
+const LoginForm = () => {
+    // const dispatch = useDispatch();
+    const handleSubmit = (values, resetForm) => {
+        // dispatch(authOperations.register(values));
+        resetForm();
+    };
+
+    return (
+        <div>
+            <Formik
+            initialValues={initialValues}
+            validationSchema={schema}
+            onSubmit={handleSubmit}
+            >
+                <Wrapper>
+                    <NameLable htmlFor={EmailInputId}>Email</NameLable>
+                    <Input
+                    type="email"
+                    name="email"
+                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                    title="Email must be a valid email address"
+                    id={EmailInputId}
+                    required
+                    />
+                    <ErrorBox><ErrorMessage name="email" render={msg => <Error>{`Please, enter Email`}</Error>}/></ErrorBox>
+                    <NameLable htmlFor={PasswordInputId}>Password</NameLable>
+                    <Input
+                    type="password"
+                    name="password"
+                    id={PasswordInputId}
+                    required
+                    />
+                    <ErrorBox><ErrorMessage name="password" render={msg => <Error>{`Please, enter Password`}</Error>}/></ErrorBox>
+                    <SubitForm type="submit" name="Log in">Log in</SubitForm>
+                </Wrapper>
+            </Formik>
+        </div>
+    )
+}
+
+export default LoginForm;
