45190db43f287bfd6ca251ddb7ec548c2c88458b
Veronika Savluk <s_veronica@ukr.net>
2022-11-01 00:27:09 +0200

    Create Login.js

---
+++ src/page/Login.js/Login.js
@@ -0,0 +1,12 @@
+import LoginForm from "components/LoginForm/LoginForm";
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Log in</h1>
+            <LoginForm />
+        </div>
+    )
+};
+
+export default Login;
