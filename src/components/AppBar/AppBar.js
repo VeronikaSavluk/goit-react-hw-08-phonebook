fdb6e12aa0a02fd9a1a97429092caadc92909fca
Veronika Savluk <s_veronica@ukr.net>
2022-11-02 02:33:31 +0200

    Update AppBar.js

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,13 +1,17 @@
+import { useSelector } from "react-redux";
 import Navigation from "components/Navigation/Navigation";
 import UserMenu from "../UserMenu/UserMenu";
 import AuthNav from "../AuthNav/AuthNav";
+import authSelectors from "../../redux/auth/selectors";
 
 const AppBar = () => {
-    <header>
-        <Navigation />
-        <UserMenu />
-        <AuthNav />
-    </header>
+    const isLoggedIn = useSelector(authSelectors.selectIsLoggedin);
+    return (
+        <header>
+            <Navigation />
+            {isLoggedIn ? <UserMenu /> : <AuthNav />}
+        </header>
+    );
 };
 
 export default AppBar;
