2c2c307868cd4cab56af5599543280223e0bb81e
Veronika Savluk <s_veronica@ukr.net>
2022-11-02 02:34:35 +0200

    Create Contacts.js

---
+++ src/page/Contacts/Contacts.js
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import contactsOperations from "redux/contacts/operations";
+import { selectIsLoading, selectError } from "redux/contacts/selectors";
+import ContactForm from "components/ContactForm";
+import FilterContactList from 'components/FilterContactListItem';
+import Loader from "components/Loader";
+import ContactList from "components/ContactList";
+import { Container, Title, LoaderBox, ErrorMessage } from "components/Container.styled";
+
+const Contacts = () => {
+    const dispatch = useDispatch();
+    const isLoading = useSelector(selectIsLoading);
+    const error = useSelector(selectError);
+
+    useEffect(() => {
+        dispatch(contactsOperations.fetchContacts());
+    }, [dispatch]);
+
+    return (
+        <Container>
+            <h1>Phonebook</h1>
+            <ContactForm />
+            <Title>Contacts</Title>
+            <FilterContactList />
+            <LoaderBox>
+                {isLoading && !error && <Loader />}
+                {error && <ErrorMessage>{error}</ErrorMessage>}
+            </LoaderBox>
+            <ContactList />
+        </Container>
+    );
+};
+
+export default Contacts;
