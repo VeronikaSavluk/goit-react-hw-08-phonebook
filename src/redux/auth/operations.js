3cf70426b98e3a8cd828d8d4206798ac51fb7b96
Veronika Savluk <s_veronica@ukr.net>
2022-11-02 02:37:19 +0200

    Update operations.js

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,19 +1,51 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
 import axios from "axios";
 
-axios.defaults.baseURL = `https://connections-api.herokuapp.com`;
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
-const register = createAsyncThunk("auth/register", async credential => {
+const token = {
+    set(token) {
+        axios.defaults.headers.common.Authorization = token;
+    },
+    unset() {
+        axios.defaults.headers.common.Authorization = "";
+    }
+};
+
+const register = createAsyncThunk("auth/register", async (credential, thunkAPI) => {
     try {
-        const { data } = await axios.post("user/signup", credential);
+        const { data } = await axios.post("/users/signup", credential);
+        token.set(data.token);
         return data;
-    } catch (error) {
-        console.log(error);
+    } catch (e) {
+        return thunkAPI.rejectWithValue(e.message);
+    }
+});
+
+const logIn = createAsyncThunk("auth/login", async (credential, thunkAPI) => {
+    try {
+        const { data } = await axios.post("/users/login", credential);
+        token.set(data.token);
+        return data;
+    } catch (e) {
+        return thunkAPI.rejectWithValue(e.message);
+    }
+});
+
+const logOut = createAsyncThunk("auth/logout", async (credential, thunkAPI) => {
+    try {
+        const { data } = await axios.post("/users/logout", credential);
+        token.unset(data.token);
+        return data;
+    } catch (e) {
+        return thunkAPI.rejectWithValue(e.message);
     }
 });
 
 const authOperations = {
-register,
+    register,
+    logIn,
+    logOut,
 };
 
 export default authOperations;
