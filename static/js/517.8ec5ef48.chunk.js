43ea67a438bfef200284b344bfedd7b51c5b4a79
VeronikaSavluk <98710326+VeronikaSavluk@users.noreply.github.com>
2022-11-04 00:16:22 +0000

    Deploying to gh-pages from @ VeronikaSavluk/goit-react-hw-08-phonebook@e490ecd9a5ef969856468bf4111c616d8094156d 🚀

--- static/js/517.8ec5ef48.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[517],{3517:function(e,o,n){n.r(o);var h=n(3329);o.default=function(){return(0,h.jsx)("div",{children:(0,h.jsx)("h1",{children:"Create your own phonebook"})})}}}]);
-//# sourceMappingURL=517.8ec5ef48.chunk.js.map
