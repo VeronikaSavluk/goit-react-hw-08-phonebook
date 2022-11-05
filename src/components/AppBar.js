a5d0c934212b52e137a47206c37830386c73fc23
Veronika Savluk <s_veronica@ukr.net>
2022-11-05 04:03:29 +0200

    Update AppBar.js

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -3,16 +3,16 @@ import Navigation from "components/Navigation";
 import UserMenu from "./UserMenu";
 import AuthNav from "./AuthNav";
 import { selectIsLoggedIn } from "../redux/auth/selectors";
-import { Box } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 
 const AppBar = () => {
     const isLoggedIn = useSelector(selectIsLoggedIn);
 
     return (
-        <Box as='header' display='flex' alignItems="center" justifyContent="space-between">
+        <Flex as='header' align='center' justify='space-between'>
             <Navigation />
             {isLoggedIn ? <UserMenu /> : <AuthNav />}
-        </Box>
+        </Flex>
     );
 };
 
