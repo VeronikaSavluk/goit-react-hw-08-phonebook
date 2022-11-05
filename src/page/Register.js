1eccea451c2d9849d8846695cb0677b7fb4f4d12
Veronika Savluk <s_veronica@ukr.net>
2022-11-05 04:06:08 +0200

    changed styles

--- src/page/Register.js
+++ src/page/Register.js
@@ -2,17 +2,18 @@ import { useSelector } from "react-redux";
 import RegisterForm from "components/RegisterForm";
 import { selectError } from "../redux/auth/selectors";
 import { ErrorMessage } from "components/Container.styled";
+import { Flex, Heading} from "@chakra-ui/react";
 
 const Register = () => {
     const error = useSelector(selectError);
 
     return (
-        <div>
-            <h1>Register</h1>
+        <Flex direction='column' align='center'>
+            <Heading as='h2' mt={90} fontSize={30} textAlign='center' noOfLines={1}>Register</Heading>
             <RegisterForm />
             {error === "User creation error" && (<ErrorMessage>User creation error/such user is already registered.</ErrorMessage>)}
-        </div>
-    )
-}
+        </Flex>
+    );
+};
 
 export default Register;
