134f58a878967cacdd13df1132f0218bb77dc212
Veronika Savluk <s_veronica@ukr.net>
2022-11-05 04:04:49 +0200

    changed styles

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,16 +1,16 @@
 import { useDispatch } from "react-redux";
 import { nanoid } from "nanoid";
 import * as yup from "yup";
-import { ErrorMessage, Formik } from "formik";
+import { Formik, Form, Field } from "formik";
 import {logIn} from "redux/auth/operations";
 import {
-    Wrapper,
-    NameLable,
+    FormControl,
+    FormLabel,
     Input,
-    ErrorBox,
-    Error,
-    SubitForm
-} from "./ContactForm/ContactForm.styled";
+    Button,
+    FormErrorMessage,
+    Flex
+} from "@chakra-ui/react";
 
 const EmailInputId = nanoid();
 const PasswordInputId = nanoid();
@@ -33,35 +33,62 @@ const LoginForm = () => {
     };
 
     return (
-        <div>
+        <Flex direction='column'
+            align='center'
+            w={300} p={10}
+            border={2}
+            borderColor='#FFFFF0'
+            borderStyle='solid'
+            borderRadius={15}
+        >
             <Formik
             initialValues={initialValues}
             validationSchema={schema}
             onSubmit={handleSubmit}
             >
-                <Wrapper>
-                    <NameLable htmlFor={EmailInputId}>Email</NameLable>
-                    <Input
-                    type="email"
-                    name="email"
-                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-                    title="Email must be a valid email address"
-                    id={EmailInputId}
-                    required
-                    />
-                    <ErrorBox><ErrorMessage name="email" render={msg => <Error>{`Please, enter Email`}</Error>}/></ErrorBox>
-                    <NameLable htmlFor={PasswordInputId}>Password</NameLable>
-                    <Input
-                    type="password"
-                    name="password"
-                    id={PasswordInputId}
-                    required
-                    />
-                    <ErrorBox><ErrorMessage name="password" render={msg => <Error>{`Please, enter Password`}</Error>}/></ErrorBox>
-                    <SubitForm type="submit" name="Log in">Log in</SubitForm>
-                </Wrapper>
+                {(props) => (
+                    <Form >
+                        <Field
+                            type="email"
+                            name="email"
+                            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                            title="Email must be a valid email address"
+                            id={EmailInputId}
+                            required
+                        >
+                            {({ field, form }) => (
+                                <FormControl isInvalid={form.errors.name && form.touched.name}>
+                                    <FormLabel htmlFor={EmailInputId} mb={10}>Email</FormLabel>
+                                    <Input {...field} mb={10} p={4} />
+                                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                                </FormControl>
+                            )}
+                        </Field>
+                        <Field
+                            type="password"
+                            name="password"
+                            id={PasswordInputId}
+                            required
+                        >
+                            {({ field, form }) => (
+                                <FormControl isInvalid={form.errors.name && form.touched.name}>
+                                    <FormLabel htmlFor={PasswordInputId} mb={10}>Password</FormLabel>
+                                    <Input {...field} mb={10} p={4} />
+                                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                                </FormControl>
+                            )}
+                        </Field>
+                        <Button p={2} ml='auto' mb={10}
+                            w={100} fontWeight='900'
+                            border={1} borderStyle='solid'
+                            borderColor='#f1b61ff1' borderRadius={15}
+                            bg='#f1b61ff1' color='#4d4c4c'
+                            type="submit" name="Log in">Log in
+                        </Button>
+                    </Form>
+                )}
             </Formik>
-        </div>
+        </Flex>
     )
 }
 
