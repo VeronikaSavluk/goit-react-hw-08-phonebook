d1fd667886fd8568ef87c2ebab0b1fce40636896
Veronika Savluk <s_veronica@ukr.net>
2022-11-05 04:05:46 +0200

    changed styles

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,13 +1,13 @@
 import ThemeImage from "../images/phonebook.png";
-import { Heading } from "@chakra-ui/react";
+import { Flex, Heading, Img } from "@chakra-ui/react";
 
 const Home = () => {
     return (
-        <div>
-            <Heading as='h1' fontSize={46} noOfLines={[1, 2, 3]}>Create your own phonebook</Heading>
-            <img src={ThemeImage} alt="" width={500}/>
-        </div>
-    )
-}
+        <Flex direction='column' align='center'>
+            <Heading as='h1' mt={90} fontSize={36} textAlign='center' noOfLines={2} >Create your own phonebook</Heading>
+            <Img boxSize={[270, 720, 900]} objectFit='cover' src={ThemeImage} alt=''/>
+        </Flex>
+    );
+};
 
 export default Home;
