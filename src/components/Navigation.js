f5e9402c958c6bec226a7286dacf9342cfabad6e
Veronika Savluk <s_veronica@ukr.net>
2022-11-08 20:26:18 +0200

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,18 +2,28 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { selectIsLoggedIn } from 'redux/auth/selectors';
-import { List, ListItem } from '@chakra-ui/react';
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink
+} from '@chakra-ui/react';
 
 export const Navigation = () => {
     const isLoggedIn = useSelector(selectIsLoggedIn);
 
     return (
-        <List as='nav' fontWeight='bold' fontSize='xl'>
-            <ListItem as={NavLink} to='/' mr={15}>Home</ListItem>
-            {isLoggedIn && (
-            <ListItem as={NavLink} to='/contacts'>Contacts</ListItem>
-            )}
-        </List>
+        <Breadcrumb fontWeight='bold' fontSize='xl' separator=' '>
+        <BreadcrumbItem>
+            <BreadcrumbLink as={NavLink} to='/' isCurrentPage>
+            Home
+            </BreadcrumbLink>
+        </BreadcrumbItem>
+        {isLoggedIn && <BreadcrumbItem>
+            <BreadcrumbLink as={NavLink} to='/login'>
+            Contacts
+            </BreadcrumbLink>
+        </BreadcrumbItem>}
+        </Breadcrumb>
     );
 };
 
