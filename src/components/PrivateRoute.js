82deb4f3e0ff1b826fddbd16c380fb5c00493712
Veronika Savluk <s_veronica@ukr.net>
2022-11-08 03:56:19 +0200

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -7,7 +7,7 @@ export const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
     const isLoggedIn = useSelector(selectIsLoggedIn);
     const isRefreshing = useSelector(selectIsRefreshing);
     const shouldRedirect = !isLoggedIn && !isRefreshing;
-    return shouldRedirect ? <Navigate to={ redirectTo } /> : Component;
+    return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };
 
 PrivateRoute.propTypes = {
