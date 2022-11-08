4525fc16c4ad449193e08f861584adbfacccd2d0
Veronika Savluk <s_veronica@ukr.net>
2022-11-08 02:04:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter to='/goit-react-hw-08-phonebook'>
           <ChakraProvider theme={theme}>
             <App />
           </ChakraProvider>
